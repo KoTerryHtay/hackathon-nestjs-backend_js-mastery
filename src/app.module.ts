cce610030047db80d6eeb6cb5ec45202e19de805
KoTerryHtay <luaye1990@gmail.com>
2026-07-19 18:31:20 +0630

    configure prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArcjetSecurityModule } from './lib/arcjet/arcjet.module';
+import { PrismaModule } from './lib/database/prisma.module';
 
 @Module({
-  imports: [ArcjetSecurityModule],
+  imports: [ArcjetSecurityModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
